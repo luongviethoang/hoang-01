4c536855b31d4cdc15ca32f024a2411dddc203d7
your name <hoangckhg170@gmail.com>
2021-09-30 09:27:17 +0700

    Create bai 3cpp.cpp

---
+++ bai 3cpp.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num,x;
+	printf("Nhap lan luot hai so", num,x);
+	scanf("%d %d",&num,&x);  
+	printf("hieu cua hai so do la:%d\n",num-x);
+	printf("tong cua hai so la:%d\n", num+x);
+	printf("thuong cua hai so la:%d", num/x);
+	return 0;
+}
+	

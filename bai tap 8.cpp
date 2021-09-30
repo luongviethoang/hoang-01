0d0b93bb53d8dce60f301976a9bb4b47be8a85ec
your name <hoangckhg170@gmail.com>
2021-09-30 09:27:47 +0700

    Create bai tap 8.cpp

---
+++ bai tap 8.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Kieu int: 4 Byte\n");
+	printf("So nguyen: 14\n\n");
+	printf("Kieu float: 4 Byte\n");
+	printf("So thuc kieu float: 4,21312\n\n");
+	printf("Kieu double: 8 Byte\n");
+	printf("so thuc kieu double: 3.234234234\n\n");
+	printf("Kieu char: 1 Byte\n");
+	printf("Ky tu: E\n\n");
+	printf("kieu long int: 4 Byte\n");
+	printf("Kieu long double:12 Byte");
+	return 0;
+}

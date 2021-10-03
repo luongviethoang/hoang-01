e93d67b4cc1c46354b1b5a6ffe2765c22770d38a
your name <hoangckhg170@gmail.com>
2021-10-03 22:43:40 +0700

    Update tr2ss8.c

--- tr2ss8.c
+++ tr2ss8.c
@@ -4,51 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char a;
-	printf("Nhap 1 ki tu bat ki: ");
-	scanf("%c",&a);
-	
-	switch(a){
-		case 'A':
-		case 'a':
-			 printf("Ada");
-		break;
-		
-		case 'B':
-		case 'b':
-			printf("Basic");
-		break;
-		
-		case 'C':
-		case 'c':
-			printf("COBOL");
-		break;
-		
-		case 'D':
-		case 'd':
-			printf("dBASE III");
-		break;
-				
-		case 'F':
-		case 'f':
-			printf("Fortran");
-		break;
-		
-			
-		case 'P':
-		case 'p':
-			printf("Pascal");
-		break;
-		
-			
-		case 'V':
-		case 'v':
-			printf("Visual C++");
-		break;
-		
-	   	default: 
-		   printf("sai rau buon ");
-		break;		
- }
+	int a, b, c;
+	printf("nhap 3 so can kiem tra\n");
+	scanf("%d %d %d",&a,&b,&c);
+	if ( a > b && a > c )
+      printf("%d la so lon nhat.", a);
+   else if ( b > a && b > c )
+      printf("%d la so lon nhat.", b);
+   else if ( c > a && c > b )
+      printf("%d la so lon nhat.", c);
+   else   
+      printf("Gia tri cua cac so la khong duy nhat");
+
+    
 	return 0;
 }

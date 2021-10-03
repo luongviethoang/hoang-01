71f8d271e71ea2fcc53fca699c66f88176955ca2
your name <hoangckhg170@gmail.com>
2021-10-03 14:30:24 +0700

    Create vd5ss7.cpp

---
+++ vd5ss7.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x,y;
+	x=y=0;
+	printf("enter choice(1-3):");
+	scanf("%d",&x);
+	if(x==1)
+	{
+		printf("\n enter value for y is :%d",y);
+		scanf("%d",&y);
+		if (y<=5)
+		printf("\nthe value for y is : %d",y);
+		else
+		printf("\nthe value of y exceeds 5");
+		
+	}
+	else
+	printf("\nchoice u entered was not 1");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	

2cf4d17ce42572f307e441e7d96c44fae761cb70
your name <hoangckhg170@gmail.com>
2021-10-03 14:31:04 +0700

    Create tr1.c

---
+++ tr1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	int b;
+	int pr;
+	printf(" nhap 2 so bat ki\n");
+
+	scanf("%d %d",&a,&b);
+	pr=a*b;
+	if("pr>1000")
+	printf("lon hon 1000\n");
+	else
+	printf("bang 1000");
+	return 0;
+}

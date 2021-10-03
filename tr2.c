23aeefd0178888c94e298da21cc7a01fd600c787
your name <hoangckhg170@gmail.com>
2021-10-03 14:31:10 +0700

    Create tr2.c

---
+++ tr2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	int b;
+	printf(" nhap 2 so bat ki\n");
+	scanf("%d %d",&a,&b);
+	if(a=b)
+	printf("difference is equal to values\n");
+	else
+	printf("difference is not equal to any of the values");
+	return 0;
+}

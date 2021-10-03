00cfb55629481608b17d7d0f2bc1c76f070411c9
your name <hoangckhg170@gmail.com>
2021-10-03 14:30:30 +0700

    Create vd4ss7.cpp

---
+++ vd4ss7.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x;
+	x=0;
+	printf("Enter choice (1-3):");
+	scanf("%d", &x);
+	if(x==1)
+	printf("\n choice is 1");
+	else if (x==2)
+	printf("\n choice is 2");
+	else if (x==3)
+	printf("\n choice is 3");
+	else
+	printf("\ninvalid choice");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
